338a387c514e250ae2f425de7178df9f0e088d0d
Ahmed Abbasi <aabbasi@ncstech.com>
2018-03-19 19:03:08 -0400

    Ahmed Changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,13 @@
 
+
 $(document).ready (function(){
     $("#go").on("click",function(event){
       event.preventDefault();
       var channelName=$("#channel").val();
       console.log(channelName)
-      $(".artist-name").append(channelName);
+      $(".artist-name").html(channelName);
       
-        var queryURL="https://www.googleapis.com/youtube/v3/search?part=snippet&q="+channelName+"&key=AIzaSyC30mnkcq5ly-Nr-kHMh0f05WwzzVtuO3Q&maxResults=3"
+        var queryURL="https://www.googleapis.com/youtube/v3/search?part=snippet&q="+channelName+"&key=AIzaSyC30mnkcq5ly-Nr-kHMh0f05WwzzVtuO3Q&maxResults=10"
    
        $.ajax({
         url: queryURL,
@@ -14,12 +15,14 @@ $(document).ready (function(){
       }).then(function(response) {
         console.log(response);
         var result= response;
-        var vidId1=result.items[0].id.videoId;
-        var object1='<iframe src =\"http://www.youtube.com/embed/' +vidId1+ '\"></iframe>';
-        var vidId2=result.items[1].id.videoId
-        var object2='<iframe src =\"http://www.youtube.com/embed/' +vidId2+ '\"></iframe>';
-        $("#results1").append(object1);
-        $("#results2").append(object2);
+        var vidId1=result.items[8].id.videoId;
+        var object1='<iframe width=\"85%\" height=\"100%\" src =\"http://www.youtube.com/embed/' +vidId1+ '\"></iframe>';
+        console.log(object1);
+        var vidId2=result.items[9].id.videoId
+        var object2='<iframe width=\"85%\" height=\"100%\"  src =\"http://www.youtube.com/embed/' +vidId2+ '\"></iframe>';
+        console.log(object2);
+        $("#results1").html(object1);
+        $("#results2").html(object2);
       })
     })
 }) 
